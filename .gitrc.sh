6952bd5699d870094cf423ecdb008b9bd060d2c2
piratematt <pirate-matt@users.noreply.github.com>
2024-08-22 11:55:51 -0400

    Added git diff alias

--- .gitrc.sh
+++ .gitrc.sh
@@ -2,6 +2,8 @@
 # Shortened frequent commands
 alias gs='git status';
 alias gfr='echo "git fetch --prune && git rebase"; git fetch --prune && git rebase';
+# I don't care about whitespace, nor package-lock.json changes
+alias gd="git diff -w -- . ':(exclude)package-lock.json' ':(exclude)package-lock.json'";
 
 # Fix GPG Signing
 # when gpg siging fails read this —> https://gist.github.com/paolocarrasco/18ca8fe6e63490ae1be23e84a7039374?permalink_comment_id=3767413#gistcomment-3767413
